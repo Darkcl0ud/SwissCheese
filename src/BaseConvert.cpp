84f6f9410bc1e67449f3fb1145cdcb972f2e8db8
Warren <wjbrown831989@gmail.com>
2020-08-30 17:49:23 -0400

    Initial Conversions Complete

--- src/BaseConvert.cpp
+++ src/BaseConvert.cpp
@@ -9,17 +9,30 @@ void BaseConversion(ConversionParameters InParams)
 
 	if (InParams.ModeA == 0)  /* Binary Input */
 	{
-		std::cout << std::hex << (std::bitset<64> (InParams.InString)).to_ullong() << std::endl; //works
-		std::cout << std::dec << (std::bitset<64> (InParams.InString)).to_ullong() << std::endl; //works
+		std::cout << std::hex << (std::bitset<64> (InParams.InString)).to_ullong() << std::endl;	/* Print Hex */
+		std::cout << std::dec << (std::bitset<64> (InParams.InString)).to_ullong() << std::endl;	/* Print Dec */
 	}
 	else if (InParams.ModeA == 1)  /* Decimal Input */
 	{
-		std::string binary = std::bitset<64>(std::stoi(InParams.InString)).to_string();
-		std::cout << binary << std::endl;
-		std::cout << std::hex << (std::bitset<64> (binary)).to_ullong() << std::endl; //works
+		long long out;
+		std::stringstream stream;
+
+		stream << std::dec << InParams.InString;
+		stream >> out;
+
+		std::cout << std::bitset<64>(out).to_string() << "\n";	/* Print Binary */
+		std::cout << std::hex << (std::bitset<64> (out)).to_ullong() << std::endl;	/* Print Hex */
 	}
 	else if (InParams.ModeA == 2)	/* Hexadecimal Input */
 	{
+		long long out;
+		std::stringstream stream;
+
+		stream << std::hex << InParams.InString;
+		stream >> out;
+
+		std::cout << out << std::endl;	/* Print Dec */
+		std::cout << std::bitset<64>(out).to_string(); /* Prints Binary */
 	}
 
 }
--- src/BaseConvert.h
+++ src/BaseConvert.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <bitset>
+#include <sstream>
 
 enum ConversionModes {
 	Binary,
